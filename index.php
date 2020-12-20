42ceabfa66ccae46e71dc8d02a344bde81b91f60
CharliB <barros.rcarlos@gmail.com>
2020-12-20 12:26:36 -0300

    add index

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php  
+
+    require 'config/config.php';
+    $producto = new producto();
+    $productos = $producto->listarProductos();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Catálogo de productos</h1>
+
+        <section class="row">
+
+<?php
+        foreach ( $productos as $producto ){
+?>
+            <div class="card col-lg-4 col-md-6 col-sm-12">
+                <img src="productos/<?= $producto['prdImagen'] ?>" class="card-img-top img-thumbnail">
+                <h2><?= $producto['prdNombre'] ?></h2>
+                <?= $producto['catNombre'] ?> - <?= $producto['mkNombre'] ?>
+                <br>
+                $<?= $producto['prdPrecio'] ?>
+                <a href="" class="btn btn-outline-info">
+                    ver detalle
+                </a>
+            </div>
+<?php
+        }
+?>
+
+        </section>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
